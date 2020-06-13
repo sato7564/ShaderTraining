7b72a8effa80c8743dd7f7d273ad7a60da8cb5be
sato7564 <58668848+sato7564@users.noreply.github.com>
2020-06-14 02:29:29 +0900

    クリア！

---
+++ ShaderTraining/Assets/モノクロ/PostEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PostEffect : MonoBehaviour
+{
+
+    public Material monotone;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, monotone);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ShaderTraining/Assets/ワイプ/Wipe.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wipe : MonoBehaviour
+{
+
+	public Material wipeCircle;
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		Graphics.Blit(src, dest, wipeCircle);
+	}
+}
